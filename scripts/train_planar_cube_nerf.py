3c825295b20ce642c88105ba834b9b3355782a66
nepfaff <nep19@ic.ac.uk>
2023-05-07 11:08:38 -0400

    Add config to train_planar_cube_nerf.py

--- scripts/train_planar_cube_nerf.py
+++ scripts/train_planar_cube_nerf.py
@@ -4,6 +4,8 @@ import einops
 import time
 import os
 import shutil
+from argparse import Namespace
+import json
 
 import wandb
 
@@ -15,12 +17,28 @@ from state_encoder_3d.models import (
 from state_encoder_3d.dataset import PlanarCubeDataset
 from state_encoder_3d.utils import plot_output_ground_truth
 
-OUT_PATH = f"outputs/planar_cube_nerf_{time.strftime('%Y-%b-%d-%H-%M-%S')}/checkpoints"
+config = Namespace(
+    log_path=f"outputs/planar_cube_ae_{time.strftime('%Y-%b-%d-%H-%M-%S')}",
+    checkpoint_path=f"outputs/planar_cube_ae_{time.strftime('%Y-%b-%d-%H-%M-%S')}/checkpoints",
+    data_path="data/planar_cube_grid_blue_floor.zarr",
+    batch_size=2,
+    latent_dim=256,
+    resnet_out_dim=2048,
+    lr=5e-4,
+    img_res=(64, 64),
+    near=4.0,
+    far=13.0,
+    num_samples_per_ray=250,
+    num_steps=500001,
+    steps_til_summary=1000,
+    steps_til_plot=5000,
+    wandb_mode="offline",
+)
 
 
 def save(name, step, model, optim):
     save_name = f"{name}_{step}"
-    path = os.path.join(OUT_PATH, save_name)
+    path = os.path.join(config.checkpoint_path, save_name)
     torch.save(
         {
             "trainer_state_dict": model.state_dict(),
@@ -39,8 +57,11 @@ def main():
         mode="offline",
     )
 
-    if not os.path.exists(OUT_PATH):
-        os.makedirs(OUT_PATH)
+    if not os.path.exists(config.checkpoint_path):
+        os.makedirs(config.checkpoint_path)
+
+    with open(os.path.join(config.log_path, "config.json"), "w") as fp:
+        json.dump(vars(config), fp)
 
     if torch.cuda.is_available():
         device = torch.device("cuda:0")
@@ -49,36 +70,38 @@ def main():
         device = torch.device("cpu")
     print(f"Using device {device}")
 
-    batch_size = 2
     dataset = PlanarCubeDataset(
-        data_store_path="data/planar_cube_grid.zarr",
+        data_store_path=config.data_path,
         max_num_instances=1,
         num_views=1,
     )
-    dataloader = iter(torch.utils.data.DataLoader(dataset, batch_size=batch_size))
+    dataloader = iter(
+        torch.utils.data.DataLoader(dataset, batch_size=config.batch_size)
+    )
 
-    latent_dim = 256
-    nerf = LatentNeRF(latent_ch=latent_dim).to(device)
+    nerf = LatentNeRF(latent_ch=config.latent_dim).to(device)
     nerf.apply(init_weights_normal)
     # Near and far based on z_distances in PlanarCubeEnvironment
-    renderer = VolumeRenderer(near=4, far=13, n_samples=100, white_back=True).to(device)
+    renderer = VolumeRenderer(
+        near=config.near,
+        far=config.far,
+        n_samples=config.num_samples_per_ray,
+        white_back=True,
+    ).to(device)
 
-    optim = torch.optim.Adam(nerf.parameters(), lr=5e-4, betas=(0.9, 0.999))
+    optim = torch.optim.Adam(nerf.parameters(), lr=config.lr, betas=(0.9, 0.999))
 
     img2mse = lambda x, y: torch.mean((x - y) ** 2)
 
     # Constant latent as we have a single scene
-    latent = 0.1 * torch.rand((1, latent_dim), device=device)
-    latent = einops.repeat(latent, "b ... -> (repeat b) ...", repeat=batch_size)
+    latent = 0.1 * torch.rand((1, config.latent_dim), device=device)
+    latent = einops.repeat(latent, "b ... -> (repeat b) ...", repeat=config.batch_size)
 
-    num_steps = 100001
-    steps_til_summary = 1000
-    steps_til_plot = 5000
-    for step in tqdm(range(num_steps)):
+    for step in tqdm(range(config.num_steps)):
         model_input, gt_image = next(dataloader)
         xy_pix = model_input["x_pix"].to(device)
         intrinsics = model_input["intrinsics"].to(device)
-        c2w = model_input["cam2world"].reshape(batch_size, 4, 4).to(device)
+        c2w = model_input["cam2world"].reshape(config.batch_size, 4, 4).to(device)
 
         rgb, depth = renderer(c2w, intrinsics, xy_pix, nerf, latent)
 
@@ -89,19 +112,22 @@ def main():
         loss.backward()
         optim.step()
 
-        if not step % steps_til_summary:
+        if not step % config.steps_til_summary:
             print(f"Step {step}: loss = {float(loss.detach().cpu()):.5f}")
 
             # Remove old checkpoints
-            shutil.rmtree(OUT_PATH)
-            os.mkdir(OUT_PATH)
+            shutil.rmtree(config.checkpoint_path)
+            os.mkdir(config.checkpoint_path)
 
             # Save new weights
             save(name="nerf", step=step, model=nerf, optim=optim)
 
-        if not step % steps_til_plot:
+        if not step % config.steps_til_plot:
             fig = plot_output_ground_truth(
-                rgb[0], depth[0], gt_image[0], resolution=(64, 64, 3)
+                rgb[0],
+                depth[0],
+                gt_image[0],
+                resolution=(config.img_res[0], config.img_res[1], 3),
             )
             wandb.log({f"step_{step}": fig})
 
