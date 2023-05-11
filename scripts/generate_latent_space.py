8f3289520392c91da1c352a189d20dd542bc36a3
Nicholas Pfaff <contact@nicholaspfaff.ch>
2023-05-11 18:38:24 -0400

    Add script for generating latent space

---
+++ scripts/generate_latent_space.py
@@ -0,0 +1,145 @@
+import argparse
+import os
+import shutil
+
+import zarr
+import numpy as np
+import torch
+from torch import nn
+from tqdm import tqdm
+
+from state_encoder_3d.models import (
+    CompNeRFStateEncoder,
+    CompNeRFImageEncoder,
+)
+from state_encoder_3d.dataset import PlanarCubeDataset
+
+
+def load(model: nn.Module, ckpt_path: str, device: torch.device) -> None:
+    ckpt = torch.load(ckpt_path, map_location=device)
+    model.load_state_dict(ckpt["trainer_state_dict"])
+
+
+def set_requires_grad(model: nn.Module, requires_grad: bool) -> None:
+    for param in model.parameters():
+        param.requires_grad = requires_grad
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data",
+        type=str,
+        required=True,
+        help="Path to the planar cube data zarr file.",
+    )
+    parser.add_argument(
+        "--encoder",
+        type=str,
+        required=True,
+        choices=["state", "image"],
+        help="The encoder model to use for generating the latents.",
+    )
+    parser.add_argument(
+        "--ckpt_path", type=str, required=True, help="Path to the encoder checkpoint."
+    )
+    parser.add_argument(
+        "--out", type=str, required=True, help="Path to output zarr file."
+    )
+    parser.add_argument(
+        "--latent_dim",
+        default=256,
+        type=int,
+    )
+    parser.add_argument(
+        "--resnet_out_dim",
+        default=2048,
+        type=int,
+    )
+    parser.add_argument(
+        "--num_views",
+        default=10,
+        type=int,
+    )
+    args = parser.parse_args()
+    data_path = args.data
+    encoder_name = args.encoder
+    ckpt_path = args.ckpt_path
+    out_path = args.out
+    latent_dim = args.latent_dim
+    resnet_out_dim = args.resnet_out_dim
+    num_views = args.num_views
+
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
+        torch.cuda.set_device(device)
+    else:
+        device = torch.device("cpu")
+    print(f"Using device {device}")
+
+    if encoder_name == "state":
+        encoder = CompNeRFStateEncoder(
+            out_ch=latent_dim,
+            in_ch=3,
+            resnet_out_dim=resnet_out_dim,
+        ).to(device)
+    elif encoder_name == "image":
+        encoder = CompNeRFImageEncoder(
+            out_ch=latent_dim,
+            in_ch=3,
+            resnet_out_dim=resnet_out_dim,
+            normalize=True,
+        ).to(device)
+
+    load(encoder, ckpt_path, device)
+    set_requires_grad(encoder, requires_grad=False)
+
+    dataset = PlanarCubeDataset(
+        data_store_path=data_path,
+        num_views=num_views,
+    )
+
+    # States with corresponding latents
+    states = []  # Shape (N, 4)
+    # Shape (N, num_views, latent_dim) if image encoder or (N, latent_dim) if state
+    # encoder
+    latents = []
+    for i in tqdm(range(len(dataset))):
+        data = dataset[i]
+        c2w = data["cam2world"].to(device)
+        rgb = data["rgb"].to(device)
+        state = data["env_state"].to(device)
+
+        if encoder_name == "state":
+            encoder_input = rgb.view(1, num_views, 64, 64, 3).permute(0, 1, 4, 2, 3)
+            encoder_input_dict = {
+                "images": encoder_input,
+                "extrinsics": c2w,
+            }
+            latent = encoder(encoder_input_dict)  # Shape (1, latent_dim)
+            latent = latent.squeeze(0)  # Shape (latent_dim,)
+        elif encoder_name == "image":
+            encoder_input = rgb.view(num_views, 64, 64, 3).permute(0, 3, 1, 2)
+            latent = encoder(encoder_input)  # Shape (num_views, latent_dim)
+
+        states.append(state.detach().cpu().numpy())
+        latents.append(latent.detach().cpu().numpy())
+
+    states = np.asarray(states)
+    latents = np.asarray(latents)
+
+    if os.path.exists(out_path):
+        print(
+            f"Dataset storage path {out_path} already exists. Deleting the old dataset."
+        )
+        shutil.rmtree(out_path)
+    store = zarr.DirectoryStore(out_path)
+    root = zarr.group(store=store)
+    states_store = root.zeros_like("states", states)
+    states_store[:] = states
+    latents_store = root.zeros_like("latents", latents)
+    latents_store[:] = latents
+
+
+if __name__ == "__main__":
+    main()
--- state_encoder_3d/dataset/planar_cube_dataset.py
+++ state_encoder_3d/dataset/planar_cube_dataset.py
@@ -50,7 +50,98 @@ class PlanarCubeDataset(IterableDataset):
             self._fixed_idx = np.random.randint(0, len(self._data_store.images) - 1)
 
     def __len__(self):
-        return len(self._num_instances)
+        return self._num_instances
+    
+    def __getitem__(self, idx):
+        rgbs = np.asarray(self._data_store.images[idx])
+        if self._return_depth:
+            depths = np.asarray(self._data_store.depths[idx])
+        w2cs = np.asarray(self._data_store.world2cams, dtype=np.float32)
+        intrinsics = np.asarray(self._data_store.intrinsics, dtype=np.float32)
+        finger_positions = np.asarray(
+            self._data_store.finger_positions[idx], dtype=np.float32
+        )
+        box_positions = np.asarray(
+            self._data_store.box_positions[idx], dtype=np.float32
+        )
+        env_state = np.concatenate((finger_positions, box_positions), axis=-1)
+
+        observation_idx = (
+            np.random.randint(0, len(rgbs), size=self._num_views)
+            if self._rand_views
+            else list(range(self._num_views))
+        )
+        if self._num_views == 1:
+            rgb = skimage.img_as_float32(rgbs[observation_idx[0]])
+            if self._return_depth:
+                depth = depths[observation_idx[0]]
+        else:
+            rgb = []
+            for i in observation_idx:
+                rgb.append(skimage.img_as_float32(rgbs[i]))
+            rgb = np.stack(rgb, axis=0)
+
+            if self._return_depth:
+                depth = []
+                for i in observation_idx:
+                    depth.append(depths[i])
+                depth = np.stack(depth, axis=0)
+
+        x_pix = get_opencv_pixel_coordinates(
+            *(rgb.shape[:2] if self._num_views == 1 else rgb.shape[1:3])
+        )
+        x_pix = einops.rearrange(x_pix, "i j c -> (i j) c")
+        rgb = einops.rearrange(rgb, "... i j c -> ... (i j) c")
+        if self._return_depth:
+            depth = einops.rearrange(depth, "... i j -> ... (i j)")
+
+        if self._num_views == 1:
+            c2w = np.linalg.inv(w2cs[observation_idx])
+        else:
+            c2w = []
+            for i in observation_idx:
+                c2w.append(np.linalg.inv(w2cs[i]))
+            c2w = np.stack(c2w, axis=0)
+
+        if self._sample_neg_image:
+            # Sample a negative image from a different state but same view-point
+            # as the first observation index.
+            neg_indices = []
+            while len(neg_indices) < self._num_neg_views:
+                neg_idx = np.random.randint(0, self._num_instances - 1)
+                if neg_idx == idx or neg_idx in neg_indices:
+                    continue
+                neg_indices.append(neg_idx)
+            neg_indices = neg_indices
+            neg_rgbs = []
+            for neg_idx in neg_indices:
+                neg_rgb = np.asarray(self._data_store.images[neg_idx])[
+                    observation_idx[0]
+                ]
+                neg_rgb = skimage.img_as_float32(neg_rgb)
+                neg_rgb = einops.rearrange(neg_rgb, "... i j c -> ... (i j) c")
+                neg_rgbs.append(neg_rgb)
+            neg_rgb = np.asarray(neg_rgbs)
+
+            if self._num_neg_views == 1:
+                neg_rgb.squeeze(0)
+
+        if not self._return_depth:
+            depth = torch.tensor([])
+        if not self._sample_neg_image:
+            neg_rgb = torch.tensor([])
+            
+        model_input = {
+            "cam2world": torch.from_numpy(c2w),  # Shape (num_views,4,4)
+            "intrinsics": torch.from_numpy(intrinsics),  # Shape (4,4)
+            "x_pix": x_pix,  # Shape (i*j, c)
+            "idx": torch.tensor([idx]),
+            "rgb": torch.from_numpy(rgb),  # Shape (w*h,3) if num_views=1 else (num_views,w*h,3)
+            "neg_rgb": neg_rgb,  # Shape (w*h,3) if num_neg_views=1 else (num_neg_views,w*h,3)
+            "depth": depth,  # Shape (w*h) if num_views=1 else (num_views,w*h)
+            "env_state": torch.from_numpy(env_state),  # Shape (4,)
+        }
+        return model_input
 
     def __iter__(self, override_idx=None):
         while True:
@@ -62,94 +153,4 @@ class PlanarCubeDataset(IterableDataset):
                     if self._num_instances == 1
                     else np.random.randint(0, self._num_instances - 1)
                 )
-
-            rgbs = np.asarray(self._data_store.images[idx])
-            if self._return_depth:
-                depths = np.asarray(self._data_store.depths[idx])
-            w2cs = np.asarray(self._data_store.world2cams, dtype=np.float32)
-            intrinsics = np.asarray(self._data_store.intrinsics, dtype=np.float32)
-            finger_positions = np.asarray(
-                self._data_store.finger_positions[idx], dtype=np.float32
-            )
-            box_positions = np.asanyarray(
-                self._data_store.box_positions[idx], dtype=np.float32
-            )
-            env_state = np.concatenate((finger_positions, box_positions), axis=-1)
-
-            observation_idx = (
-                np.random.randint(0, len(rgbs), size=self._num_views)
-                if self._rand_views
-                else list(range(self._num_views))
-            )
-            if self._num_views == 1:
-                rgb = skimage.img_as_float32(rgbs[observation_idx[0]])
-                if self._return_depth:
-                    depth = depths[observation_idx[0]]
-            else:
-                rgb = []
-                for i in observation_idx:
-                    rgb.append(skimage.img_as_float32(rgbs[i]))
-                rgb = np.stack(rgb, axis=0)
-
-                if self._return_depth:
-                    depth = []
-                    for i in observation_idx:
-                        depth.append(depths[i])
-                    depth = np.stack(depth, axis=0)
-
-            x_pix = get_opencv_pixel_coordinates(
-                *(rgb.shape[:2] if self._num_views == 1 else rgb.shape[1:3])
-            )
-            x_pix = einops.rearrange(x_pix, "i j c -> (i j) c")
-            rgb = einops.rearrange(rgb, "... i j c -> ... (i j) c")
-            if self._return_depth:
-                depth = einops.rearrange(depth, "... i j -> ... (i j)")
-
-            if self._num_views == 1:
-                c2w = np.linalg.inv(w2cs[observation_idx])
-            else:
-                c2w = []
-                for i in observation_idx:
-                    c2w.append(np.linalg.inv(w2cs[i]))
-                c2w = np.stack(c2w, axis=0)
-
-            if self._sample_neg_image:
-                # Sample a negative image from a different state but same view-point
-                # as the first observation index.
-                neg_indices = []
-                while len(neg_indices) < self._num_neg_views:
-                    neg_idx = np.random.randint(0, self._num_instances - 1)
-                    if neg_idx == idx or neg_idx in neg_indices:
-                        continue
-                    neg_indices.append(neg_idx)
-                neg_indices = neg_indices
-                neg_rgbs = []
-                for neg_idx in neg_indices:
-                    neg_rgb = np.asarray(self._data_store.images[neg_idx])[
-                        observation_idx[0]
-                    ]
-                    neg_rgb = skimage.img_as_float32(neg_rgb)
-                    neg_rgb = einops.rearrange(neg_rgb, "... i j c -> ... (i j) c")
-                    neg_rgbs.append(neg_rgb)
-                neg_rgb = np.asarray(neg_rgbs)
-
-                if self._num_neg_views == 1:
-                    neg_rgb.squeeze(0)
-
-            if not self._return_depth:
-                depth = torch.tensor([])
-            if not self._sample_neg_image:
-                neg_rgb = torch.tensor([])
-
-            model_input = {
-                "cam2world": torch.from_numpy(c2w),  # Shape (num_views,4,4)
-                "intrinsics": torch.from_numpy(intrinsics),  # Shape (4,4)
-                "x_pix": x_pix,  # Shape (i*j, c)
-                "idx": torch.tensor([idx]),
-                "rgb": rgb,  # Shape (w*h,3) if num_views=1 else (num_views,w*h,3)
-                "neg_rgb": neg_rgb,  # Shape (w*h,3) if num_neg_views=1 else (num_neg_views,w*h,3)
-                "depth": depth,  # Shape (w*h) if num_views=1 else (num_views,w*h)
-                "env_state": torch.from_numpy(env_state),  # Shape (4,)
-            }
-
-            yield model_input
+            yield self.__getitem__(idx)
